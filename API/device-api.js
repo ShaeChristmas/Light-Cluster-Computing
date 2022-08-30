4c400a93816d60e6b6f30367c6eacfccf8ccdfc7
Shaine Christmas <shainechristmas@gmail.com>
2022-08-30 14:53:18 +1000

    FIX: change how undef is detected

--- API/device-api.js
+++ API/device-api.js
@@ -249,7 +249,7 @@ async function multiplyMatrices(matrixA, matrixB, number = 0) {
   pointsToUse = points.slice(curcount, points.length);
   //console.log("Points: ", pointsToUse);
   // Set each as promise
-  if (pointsToUse != undefined) {
+  if (pointsToUse[0] != undefined) {
     if(pointsToUse.length > 0) {
       promises.push(
         sendReq(ips[nodev], matrixA, pointsToUse).then((data) => {
