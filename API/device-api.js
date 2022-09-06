ebdd12c2bd1e694ae5ea91c73233a05dffe6d5cc
Shaine Christmas <shainechristmas@gmail.com>
2022-09-06 14:17:48 +1000

    FIX: Change condition for final sendReq

--- API/device-api.js
+++ API/device-api.js
@@ -248,7 +248,7 @@ async function multiplyMatrices(matrixA, matrixB, number = 0) {
   }
   //console.log("Points: ", pointsToUse);
   // Set each as promise, add to promises object
-  if (curcount == points.length) {
+  if (curcount != points.length) {
     pointsToUse = points.slice(curcount, points.length);
     console.log(nodev, ",",pointsToUse);
     promises.push(
